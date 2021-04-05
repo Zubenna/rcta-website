60a740af7d98f4fc8268ffeda82366fdedbeb5b9
Zubenna <emelunna@gmail.com>
2021-04-05 15:40:04 +0100

    Completing cover, services, counter sections

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,31 @@ $(".dropdown-menu a.dropdown-toggle").on("click", function (e) {
 
   return false;
 });
+
+
+// Header style on scroll
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle("scroll-active", windowPosition);
+});
+
+// Implement Counter
+const counters = document.querySelectorAll(".counter");
+const speed = 590;
+
+counters.forEach((count) => {
+  const updateCounter = () => {
+    const ourTarget = +count.getAttribute("data-target");
+    const ourCounter = +count.innerText;
+    const increment = ourTarget / speed;
+
+    if (ourCounter < ourTarget) {
+      count.innerText = (ourCounter + increment).toFixed(2);
+      setTimeout(updateCounter, 1);
+    } else {
+      count.innerText = ourTarget;
+    }
+  };
+  updateCounter();
+});
